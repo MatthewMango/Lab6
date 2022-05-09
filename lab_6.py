e77c098f39a43a190ae912f5490216980aab9a49
MatthewMango <98561675+MatthewMango@users.noreply.github.com>
2022-05-09 16:39:20 -0700

    finished code

--- lab_6.py
+++ lab_6.py
@@ -36,6 +36,54 @@ loop tree times
 """
 
 
+import random
+
+total_earned = 0
+
+def main():
+    for i in range(0, 3):
+        results.append(spin())
+
+    print("results", results)
+    winner = jackpot(results)
+
+    if(winner):
+        print("Winner! you win")
+    else:
+        print("Sorry try again!")
+
+    option = input("Play again? ")
+
+    if option.lower() == "y" or option.lower() == "yes":
+        main()
+
+def spin():
+    rand_num = random.randint(1, 20)
+    output = ""
+    if(rand_num > 15):
+        output = "Cherries"
+    elif(rand_num > 10):
+        output = "Orange"
+    elif(rand_num > 5):
+        output = "Plum"
+    elif(rand_num > 2):
+        output = "Bell"
+    elif(rand_num > 1):
+        output = "Melon"
+    else:
+        output = "Bar"
+
+    print(output, end=" ")
+    return output
+
+    def jackpot(results):
+
+        if(results[0] == results [1] == results[2]):
+            return True
+        return False
+
+    main()
+
 
 
 
